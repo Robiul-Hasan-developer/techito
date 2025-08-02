ba39a2fb9bd2aae28599d99b39f7df4d4f56c043
Robiul Hasan <robiulhasan9559@gmail.com>
2025-08-02 10:48:35 +0600

    (Section) Mission vision section fully done

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -348,7 +348,63 @@
     });
     // ========================= service Tab Js End ===================
 
-    
+    // ========================= Active Tab Background animation Js Start ===================
+    function moveBackground(wrapper) {
+      var $activeTab = $(wrapper).find(".active").parent("li");
+      var position = $activeTab.position();
+      var width = $activeTab.width();
+
+      $(wrapper)
+        .find(".background")
+        .css({
+          "inset-inline-start": position.left + "px",
+          width: width + "px",
+        });
+    }
+
+    // Move Background on page load for each tab group
+    $(".animate-background-wrapper").each(function () {
+      moveBackground(this);
+    });
+
+    // Move Background on tab click
+    $(".animate-background-wrapper .nav-link").on("click", function () {
+      var wrapper = $(this).closest(".animate-background-wrapper");
+      wrapper.find(".nav-link").removeClass("active");
+      $(this).addClass("active");
+      moveBackground(wrapper);
+    });
+
+    // ========================= Active Tab Background animation Js End ===================
+
+      // ========================= Animated Radial Progress Js Start ===================
+    function animateProgress() {
+      $("svg.radial-progress").each(function () {
+        // Check if the element is within the viewport
+        const elementTop = $(this).offset().top;
+        const elementBottom = elementTop + $(this).outerHeight();
+        const viewportTop = $(window).scrollTop();
+        const viewportBottom = viewportTop + $(window).height();
+
+        if (elementBottom > viewportTop && elementTop < viewportBottom) {
+          const percent = $(this).data("percentage");
+          const radius = $(this).find("circle.complete").attr("r");
+          const circumference = 2 * Math.PI * radius;
+          const strokeDashOffset =
+            circumference - (percent / 100) * circumference;
+
+          // Animate the circle
+          $(this)
+            .find("circle.complete")
+            .css("stroke-dashoffset", strokeDashOffset);
+        }
+      });
+    }
+
+    // Trigger animation on scroll and page load
+    $(window).on("scroll", animateProgress);
+    animateProgress(); // Run on page load
+    // ========================= Animated Radial Progress Js End ===================
     
     
     
